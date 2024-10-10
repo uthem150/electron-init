46bb8ecb0100a4d48b2e1c6fe3c70f5dad512ae2
uthem150 <uthem150@gmail.com>
2024-10-10 18:27:46 +0900

    reactapp에  electron적용하기

---
+++ react-electron/src/main.js
@@ -0,0 +1,22 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+  win.loadURL("http://localhost:3000");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
