02622c2c898ce0e45bea8225dbdcc67eb17cab73
uthem150 <uthem150@gmail.com>
2024-10-10 22:58:55 +0900

    기본 타이틀바 프레임 생성

--- react-electron/src/index.js
+++ react-electron/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.js";
 import reportWebVitals from "./reportWebVitals.js";
+import TitleBar from "./components/TitleBar/TitleBar.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <TitleBar />
     <App />
   </React.StrictMode>
 );
